249df45eafea9f61339c91491024248896a2cc1a
kksah2063 <kksah2063@gmail.com>
2026-01-31 20:37:18 +0545

    change code in server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => res.send("API is working..."));
+app.get("/", (req, res) => res.send("API is working.."));
 app.use("/api/contact", ContactRouter);
 
 // Port
